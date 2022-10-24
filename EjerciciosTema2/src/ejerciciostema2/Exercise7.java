5eef7abdb1dfb1a3e263ad91252bc4cb8ae53cd2
jmgallego <jmgallego@106-09.info2>
2022-10-24 10:15:48 +0200

    First commit

--- EjerciciosTema2/src/ejerciciostema2/Exercise7.java
+++ EjerciciosTema2/src/ejerciciostema2/Exercise7.java
@@ -7,9 +7,11 @@ public class Exercise7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int sec, min, hour;
+		int sec, min, hour; //Variables are used to store the hours, minutes and seconds that user enters
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Scanner is started to introduce data into variables
+		
+		//title and spaces to ask the user for the hour
 		
 		System.out.println("Introduce la hora:");
 		System.out.print("horas: ");
@@ -20,7 +22,9 @@ public class Exercise7 {
 		sec = sc.nextInt();
 		System.out.println();
 		
-		sec = sec + 1;
+		sec = sec + 1; //operation to add one second to the hour introduced
+		
+		//if seconds are 60, they convert into a minute
 		
 		if (sec == 60) {
 			
@@ -29,12 +33,16 @@ public class Exercise7 {
 		
 		}
 		
+		//if minutes are 60, they convert into a minute
+		
 		if (min >= 60) {
 				
 			hour = hour + 1;
 			min = 0;
 				
 		}
+		
+		//if the hours are 24, all transforms to 0:0:0
 		
 		if (hour == 24) {
 					
@@ -44,7 +52,9 @@ public class Exercise7 {
 				
 		}
 		
-		sc.close();
+		System.out.println("La hora dentro de un segundo es: " + hour + ":" + min + ":" + sec); //Final message printed to the user
+		
+		sc.close(); //Scanner is closed
 		
 	}
 
