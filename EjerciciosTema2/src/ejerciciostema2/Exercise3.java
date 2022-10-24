5d540322826260c9b7e1f72c7ee76fd74b3b991f
juanm <juanm@DESKTOP-CQ79OHG>
2022-10-25 00:15:04 +0200

    First commit

--- EjerciciosTema2/src/ejerciciostema2/Exercise3.java
+++ EjerciciosTema2/src/ejerciciostema2/Exercise3.java
@@ -15,28 +15,36 @@ public class Exercise3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		double buyFood;
-		int numAnimal;
-		double dailyFood;
+		double buyFood; //Variable used to store the food bought
+		int numAnimal; //Variable used to store the number of animals
+		double dailyFood; //Variables used to store the food animals daily eat
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Scanner is started to introduce data into variables
 		
+		//The user introduces the food bought
 		System.out.print("Introduzca la cantidad de comida comprada en kg: ");
 		buyFood = sc.nextDouble();
+		
+		//The user introduces the number of animals
 		System.out.print("Introduzca el número de animales: ");
 		numAnimal = sc.nextInt();
+		
+		//The user introduces all the food they eat
 		System.out.print("Introduzca la cantidad de comida que comen todos los animales en kg: ");
 		dailyFood = sc.nextDouble();
 		System.out.println();
 		
-		if (numAnimal <= 0) {
+		//if animals are 0 or less, the program will print an error
+		if (numAnimal <= 0) { 
 			
 			System.out.println("ERROR: Ha introducido una cantidad de 0 o menos animales");
 			
+		//if the food animals eat each day is less or the same than the bought one, it will print that there are enough
 		} else if (dailyFood <= buyFood) {
 			
 			System.out.println("Hay comida suficiente para todos los animales");
 			
+		//else the program will print that there aren't enough and it will calculate how many food they have to eat individually
 		} else {
 			
 			dailyFood = buyFood / numAnimal; 
@@ -45,7 +53,7 @@ public class Exercise3 {
 			
 		}
 		
-		sc.close();
+		sc.close(); //Scanner is closed
 		
 	}
 
